b424304a599d032e59bb8d4a496c9949bc4fc203
Yong Li <yong.li@intel.com>
2024-06-27 14:58:15 +0800

    since the ramdisk can't be recognized, use for loop for test

--- init/main.c
+++ init/main.c
@@ -1432,6 +1432,10 @@ static int run_init_process(const char *init_filename)
 	pr_debug("  with environment:\n");
 	for (p = envp_init; *p; p++)
 		pr_debug("    %s\n", *p);
+	while(1) {
+		 printk("-");
+		 mdelay(1000);
+	}
 	return kernel_execve(init_filename, argv_init, envp_init);
 }
 
